e3573178e02291e711048555e1de752de92b742b
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-21 16:51:05 +0530

    Update FireBaseInitialization.java

--- src/main/java/com/SXN/Vendor/FireBaseInitialization/FireBaseInitialization.java
+++ src/main/java/com/SXN/Vendor/FireBaseInitialization/FireBaseInitialization.java
@@ -13,21 +13,35 @@ import java.io.IOException;
 
 public class FireBaseInitialization {
 
+    
+     // try {
+     //        // Read service account key from environment variable
+     //        String renderToken = System.getenv("RENDER_TOKEN");
+
+     //        // Convert the service account key string to InputStream
+     //        ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(renderToken.getBytes(StandardCharsets.UTF_8));
+     //        FirebaseOptions options = new FirebaseOptions.Builder()
+     //                .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(serviceAccountStream.getBytes(StandardCharsets.UTF_8))))
+     //                .build();
+
+     //        FirebaseApp.initializeApp(options);
+     //    } catch (IOException e) {
+     //        System.err.println("Error initializing Firebase: " + e.getMessage());
+     //    }
     @PostConstruct
-     try {
-            // Read service account key from environment variable
-            String renderToken = System.getenv("RENDER_TOKEN");
+    FileInputStream serviceAccount =
+                null;
+        try {
+            serviceAccount = new FileInputStream("./serviceAccountKey.json");
 
-            // Convert the service account key string to InputStream
-            ByteArrayInputStream serviceAccountStream = new ByteArrayInputStream(renderToken.getBytes(StandardCharsets.UTF_8));
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(serviceAccountStream.getBytes(StandardCharsets.UTF_8))))
-                    .build();
 
-            FirebaseApp.initializeApp(options);
-        } catch (IOException e) {
-            System.err.println("Error initializing Firebase: " + e.getMessage());
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
 
 }
