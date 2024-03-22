b400c5b91de9f8ad7902fa251af2578a0a7a1993
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-22 12:36:14 +0530

    Add files via upload

---
+++ src/main/java/com/SXN/Vendor/Entity/VendorNames.java
@@ -0,0 +1,22 @@
+package com.SXN.Vendor.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class VendorNames {
+        private String vendors="Vendors";
+        private String vendorId;
+        private String vendorName;
+}
+
