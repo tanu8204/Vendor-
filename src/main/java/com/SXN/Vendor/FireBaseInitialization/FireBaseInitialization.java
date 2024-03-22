15b950a5ab60e5cb652b8e6c62d79396981f06d7
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-03-22 23:06:57 +0530

    Update FireBaseInitialization.java

--- src/main/java/com/SXN/Vendor/FireBaseInitialization/FireBaseInitialization.java
+++ src/main/java/com/SXN/Vendor/FireBaseInitialization/FireBaseInitialization.java
@@ -4,12 +4,14 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 
-@Configuration
+@Component
 public class FireBaseInitialization {
 
         @PostConstruct
