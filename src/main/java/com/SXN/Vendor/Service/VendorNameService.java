5f46e293b16a01edd784f1c0c25aca8b514d2d9e
tanu8204 <161141310+tanu8204@users.noreply.github.com>
2024-04-25 23:22:20 +0530

    Delete src directory

--- src/main/java/com/SXN/Vendor/Service/VendorNameService.java
+++
@@ -1,9 +0,0 @@
-package com.SXN.Vendor.Service;
-
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-public interface VendorNameService {
-
-    Map<String, Object> getVendorDetails() throws ExecutionException, InterruptedException;
-}
